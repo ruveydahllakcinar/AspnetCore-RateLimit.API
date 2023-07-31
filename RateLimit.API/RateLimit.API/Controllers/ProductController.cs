fc7a1b8b84aa4bee2afde2e782f0aa8e96a59097
Ruveyda Hilal <rhakcinar@gmail.com>
2023-07-31 14:56:18 +0300

    Rate Limit topic completed

---
+++ RateLimit.API/RateLimit.API/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+
+namespace RateLimit.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableRateLimiting("Sliding")] // Hangi politikanın bu controller üzerinde aktif olacağını belirlediğimiz bir attribute
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            return Ok();
+        }
+    }
+    public class CustomRateLimitPolicy : IRateLimiterPolicy<string>
+    {
+        public Func<OnRejectedContext, CancellationToken, ValueTask>? OnRejected =>
+        (context, cancellationToken) =>
+        {
+            //Log...
+            return new();
+        };
+
+        public RateLimitPartition<string> GetPartition(HttpContext httpContext)
+        {
+            return RateLimitPartition.GetFixedWindowLimiter("", _ => new()
+            {
+                PermitLimit = 4,
+                Window = TimeSpan.FromSeconds(12),
+                QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                QueueLimit = 2
+            });
+        }
+
+    }
+}
---
+++ RateLimit.API/RateLimit.API/Program.cs
@@ -0,0 +1,110 @@
+﻿
+
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+#region Fixed Window
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("Fixed", _options =>
+    {
+        _options.Window = TimeSpan.FromSeconds(12); //Her 12 saniyede bir yazacağımız politika geçerli olacak.
+        _options.PermitLimit = 4; //Hedr 12 saniyede bir 4 tane request gönderme hakkımız bulunmakta.
+        _options.QueueLimit = 2; // 12  saniye içerisinde 4 tane request al. Bu 4'ten fazlası gelirse kuyruğa al. 
+        _options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst; // Kuyruğa aldıklarımı eskiden yeniye doğru sıralayarak işlemeye başlıyorum.
+    });
+});
+
+#endregion 
+#region Sliding Window
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddSlidingWindowLimiter("Sliding", _options =>
+    {
+        _options.Window = TimeSpan.FromSeconds(12);
+        _options.PermitLimit = 4;
+        _options.QueueLimit = 2;
+        _options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        _options.SegmentsPerWindow = 2; // Her bir periyodun kendisinden önceki sürece ne kadarlık kota harcayacağını ifade etmektedir. Bir sonraki periyodun en fazla 2 tane hakkını kullanabilecektir.                                                                                                             
+
+    });
+});
+#endregion
+
+#region Token Bucket
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddTokenBucketLimiter("Token", _options =>
+    {
+        _options.TokenLimit = 4;
+        _options.TokensPerPeriod = 4;
+        _options.QueueLimit = 2;
+        _options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        _options.ReplenishmentPeriod = TimeSpan.FromSeconds(12); //Periyotları ne kadar süreye böleceğimi belirtiyorum.
+    });
+});
+#endregion
+
+#region Concurrency
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddConcurrencyLimiter("Concurrency", _options =>
+    {
+      
+        _options.PermitLimit = 4; 
+        _options.QueueLimit = 2; 
+        _options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst; 
+    });
+});
+
+#endregion
+
+#region OnRejected Property
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("Fixed", _options =>
+    {
+        _options.Window = TimeSpan.FromSeconds(12); //Her 12 saniyede bir yazacağımız politika geçerli olacak.
+        _options.PermitLimit = 4; //Hedr 12 saniyede bir 4 tane request gönderme hakkımız bulunmakta.
+        _options.QueueLimit = 2; // 12  saniye içerisinde 4 tane request al. Bu 4'ten fazlası gelirse kuyruğa al. 
+        _options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst; // Kuyruğa aldıklarımı eskiden yeniye doğru sıralayarak işlemeye başlıyorum.
+    });
+
+    options.OnRejected = (context, cancellationToken) =>
+    {
+        return new();
+    };
+});
+#endregion
+
+
+
+builder.Services.AddMemoryCache();
+
+
+var app = builder.Build();
+
+app.UseRateLimiter();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
